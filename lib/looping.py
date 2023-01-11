c0094314f10ff3d9512d748085ba6672d5b623af
Elijah Smith <elijahsmith.m@gmail.com>
2023-01-11 14:29:00 -0600

    looping in python

--- lib/looping.py
+++ lib/looping.py
@@ -2,12 +2,31 @@
 
 def happy_new_year():
     # code goes here
-    pass
+    i = 10
+    while i > 0:
+        print(i)
+        i = i - 1
+
+    print("Happy New Year!")
+    
+    
 
 def square_integers(int_list):
     # code goes here
-    pass
+    return [int * int for int in int_list]
 
 def fizzbuzz():
     # code goes here
-    pass
+    for i in range(100):
+        current = i + 1
+        if current % 3 == 0 and current % 5 == 0:
+            print("FizzBuzz")
+        elif current % 3 == 0:
+            print("Fizz")
+        elif current % 5 == 0:
+            print("Buzz")
+        else:
+            print(current)
+
+
+
